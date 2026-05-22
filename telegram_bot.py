0af2939e98d46f1950b6cc07bbde33b957bcd8d4
ji4ka <153725977+ji4ka@users.noreply.github.com>
2026-05-22 21:40:50 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -111,8 +111,9 @@ def run_bot():
     except Exception as e:
         print(f"Полингът спря: {e}")
 
-threading.Thread(target=run_bot, daemon=True).start()
-
 if __name__ == "__main__":
+    # Местим стартирането ТУК вътре, за да сме 100% сигурни, че се пуска само веднъж!
+    threading.Thread(target=run_bot, daemon=True).start()
+    
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
